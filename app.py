9b2cd3d596854261466c4e485d398e7e2673686e
Hepestoo <teckalan2@gmail.com>
2025-11-20 12:00:03 -0500

    commit pt5

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello from app")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hola desde mi app de flask"
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
